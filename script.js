6dd43c6759d25f33eb6fa6e919b10ddbbe0922dd
Y <yhilian.stevens@virginiogomez.cl>
2025-06-01 18:37:43 -0400

    tagetas

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=1300';
 
-let allPokemon = [];
+const pokemonListDiv = document.getElementById('pokemon-list');
+const searchInput = document.getElementById('search');
+const detailsDiv = document.getElementById('pokemon-details');
 
+let allPokemon = [];
 async function fetchAllPokemon() {
   const response = await fetch(API_URL);
   const data = await response.json();
   allPokemon = data.results;
+  renderPokemonList(allPokemon);
+}
+function renderPokemonList(pokemonArray) {
+  pokemonListDiv.innerHTML = '';
+  pokemonArray.forEach(pokemon => {
 
-  console.log("Pokémon:");
-  allPokemon.forEach(pokemon => {
-    console.log(pokemon.name);
+
+    const col = document.createElement('div');
+    col.className = 'col';
+    col.innerHTML = `
+      <div class="card h-200 text-center shadow">
+
+        <div class="card-body">
+          <h5 class="card-title text-capitalize">${pokemon.name}</h5>
+          <button class="btn btn-primary" onclick="fetchPokemonDetails('${pokemon.url}')">Ver detalles</button>
+        </div>
+      </div>
+    `;
+    pokemonListDiv.appendChild(col);
   });
 }
-
 
 fetchAllPokemon();
